792008838ce13ce164ffe37489b69d86c17eeed2
EmmanuelZiviello <124155626+EmmanuelZiviello@users.noreply.github.com>
2025-03-11 16:23:04 +0100

    aggiusto controller perchè manca implementazione

--- F_taste_richieste/controllers/richieste_controller.py
+++ F_taste_richieste/controllers/richieste_controller.py
@@ -3,7 +3,7 @@ from flask_restx import Resource,fields
 from flask_jwt_extended import get_jwt_identity
 from F_taste_richieste.utils.jwt_custom_decorators import paziente_required
 from F_taste_richieste.schemas.richiesta_aggiunta_paziente import RichiestaAggiuntaPazienteSchema
-from F_taste_richieste.services.richieste_service import RichiestaAggiuntaPazienteService
+from F_taste_richieste.services.richieste_service import RichiesteService
 from F_taste_richieste.namespaces import paziente_ns
 
 gestisciRichiestaRequestModel = paziente_ns.model('gestisci Richiesta Request Model', {
@@ -23,7 +23,7 @@ class RichiestaAggiuntaPaziente(Resource):
     @paziente_required()
     def get(self):
         identity = get_jwt_identity()
-        return RichiestaAggiuntaPazienteService.get_richieste_utente(identity)
+       # return RichiestaAggiuntaPazienteService.get_richieste_utente(identity)
 
     @paziente_ns.expect(gestisciRichiestaRequestModel)
     @paziente_ns.doc('accetta/revoca la richiesta di aggiunta')
@@ -33,10 +33,10 @@ class RichiestaAggiuntaPaziente(Resource):
         json = request.get_json()
         if not json:
             return {"message": "Dati non forniti"}, 400
-        return RichiestaAggiuntaPazienteService.gestisci_richiesta(identity, json['fk_nutrizionista'], json['conferma'])
+       # return RichiestaAggiuntaPazienteService.gestisci_richiesta(identity, json['fk_nutrizionista'], json['conferma'])
 
     @paziente_required()
     @paziente_ns.doc('elimina la condivisione con il tuo nutrizionista')
     def delete(self):
         identity = get_jwt_identity()
-        return RichiestaAggiuntaPazienteService.revoca_condivisione(identity)
+        #return RichiestaAggiuntaPazienteService.revoca_condivisione(identity)
